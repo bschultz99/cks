4473d1368227663c7048770d9a74ecdf6b65a474
Bryant Schultz <bryantschultz99@gmail.com>
2024-10-03 14:21:56 -0500

    Database Connection

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from flask import Flask
+import os, psycopg2
 
 app = Flask(__name__)
 
 
 
 if __name__ == "__main__":
+    conn = psycopg2.connect(database=os.getenv("PGDATABASE"),
+                            host=os.getenv("PGHOST"),
+                            user=os.getenv("POSTGRES_USER"),
+                            password=os.getenv("POSTGRES_PASSWORD"),
+                            port=os.getenv("PGPORT"))
+    cursor = conn.cursor()
     app.run(debug=True, port=8080)
