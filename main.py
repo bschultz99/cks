8fc3d6b656363b27e906500100d0bcf75203a939
Bryant Schultz <bryantschultz99@gmail.com>
2024-10-06 15:44:29 -0500

    Password

--- main.py
+++ main.py
@@ -4,8 +4,27 @@ import os, psycopg2
 import secrets
 import string
 import csv
+import bcrypt
 
 app = Flask(__name__)
+
+# Helper Methods
+def generate_password():
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(secrets.choice(characters) for _ in range(12))
+    return password
+
+def hash_password(password):
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
+
+def check_password(input_password, stored_password):
+    if bcrypt.checkpw(input_password.encode('utf-8'), stored_password):
+        return True
+    return False
+
+
 
 
 # Applicant
@@ -13,17 +32,13 @@ app = Flask(__name__)
 @app.route('/newapplicant', methods=['GET'])
 def newApplicant():
     """Create a new applicant in the database and send them an email to fill out initial information."""
-    characters = string.ascii_letters + string.digits + string.punctuation
     with open('./Settings/new_applicants.csv', mode='r', newline='') as file:
         reader = csv.reader(file)
         for row in reader:
-            password = ''.join(secrets.choice(characters) for _ in range(12))
-            print(password)
+           password = generate_password()
+           pass_hash = hash_password(password)
+           print(check_password(pass_hash, hash_password(password)))
     return Response(), 200
-
-
-
-
 
 
 
