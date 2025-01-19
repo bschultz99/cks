d3a831a789ec9b43fba498d85856d571535361e7
Bryant Schultz <bryantschultz99@gmail.com>
2025-01-19 13:17:20 -0600

    Update main.py

--- main.py
+++ main.py
@@ -77,7 +77,8 @@ if __name__ == "__main__":
                             password=os.getenv("POSTGRES_PASSWORD"),
                             port=os.getenv("PGPORT"))
     cursor = conn.cursor()
-    cursor.execute("DROP TABLE IF EXISTS applications;")
+    cursor.execute("DROP TABLE IF EXISTS applications CASCADE;")
+    conn.commit()
     cursor.execute(CREATE_TABLES)
     conn.commit()
     app.run(debug=True, host='0.0.0.0', port=8080)
