68aab35c5006a763bafbe46bda734f67928bc48e
Bryant Schultz <bryantschultz99@gmail.com>
2025-01-20 09:28:46 -0600

    Update main.py

--- main.py
+++ main.py
@@ -126,4 +126,4 @@ if __name__ == "__main__":
         print("Tables Created")
         #newApplicant()
         code_executed = True
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=False, host='0.0.0.0', port=8080)
