5fb02520e654a2d7721a6049437c102d715fe4a0
Bryant Schultz <bryantschultz99@gmail.com>
2025-01-20 13:12:53 -0600

    Update main.py

--- main.py
+++ main.py
@@ -139,6 +139,5 @@ if __name__ == "__main__":
         cursor = conn.cursor()
         cursor.execute(CREATE_TABLES)
         conn.commit()
-        print(send_email())
         code_executed = True
     app.run(debug=False, host='0.0.0.0', port=8080)
