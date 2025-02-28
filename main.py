a6907f88d6628259cb60d104f8ecad9162604867
Bryant Schultz <bryantschultz99@gmail.com>
2025-02-28 12:06:38 -0600

    Update main.py

--- main.py
+++ main.py
@@ -368,8 +368,9 @@ if __name__ == "__main__":
                                 password=os.getenv("POSTGRES_PASSWORD"),
                                 port=os.getenv("PGPORT"))
         cursor = conn.cursor()
-        #cursor.execute("DROP TABLE IF EXISTS applications CASCADE;")
-        #conn.commit()
+        cursor.execute("DROP TABLE IF EXISTS key_values;")
+        cursor.execute("Insert into key_values (scholarship_total, start_date, end_date) values (46000, '2025-03-01', '2025-04-01');")
+        conn.commit()
         cursor.execute(CREATE_TABLES)
         conn.commit()
         code_executed = True
