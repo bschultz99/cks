0c733ac65f8ec2d10612f3e25b55587c8d35f3ac
Bryant Schultz <bryantschultz99@gmail.com>
2025-02-28 12:32:06 -0600

    admin

--- main.py
+++ main.py
@@ -350,6 +350,10 @@ def saveApplication():
 def index():
     return render_template('index.html')
 
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
 @app.route('/error')
 def error():
     error_message = request.args.get('message', 'An error occurred.')
