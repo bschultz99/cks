8ee7ab97eba1141276fdce9ca905aed53642ae2a
Bryant Schultz <bryantschultz99@gmail.com>
2025-03-06 18:37:33 -0600

    Update main.py

--- main.py
+++ main.py
@@ -390,7 +390,7 @@ if __name__ == "__main__":
                                 password=os.getenv("POSTGRES_PASSWORD"),
                                 port=os.getenv("PGPORT"))
         cursor = conn.cursor()
-        cursor.execute("DROP TABLE IF EXISTS applications CASCADE;")
+        cursor.execute("UPDATE key_values SET start_date = '2025-03-06', end_date = '2025-04-06';")
         conn.commit()
         cursor.execute(CREATE_TABLES)
         conn.commit()
